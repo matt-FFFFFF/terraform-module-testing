8d02cb9fd5a9c825162c8338fced09ed38b55ffe
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-04-20 16:55:31 +0100

    refactor: subtests and any type

--- level_01/test/module_test.go
+++ level_01/test/module_test.go
@@ -32,34 +32,52 @@ func TestCondition(t *testing.T) {
 	t.Parallel()
 
 	// Define module variables
-	// See https://go.dev/tour/methods/14 for a description of the empty interface.
-	// Search the internet for 'golang empty interface' for more examples.
-	vars := map[string]interface{}{
+	// See https://go.dev/tour/methods/14 for a description of the any value (aka empty interface).
+	// This is a good article: https://bitfieldconsulting.com/golang/map-string-interface.
+
+	// First define a map of strings to any value, these will contain the terraform variables passed to the module
+	// The map key (string) is the name of the variable, the value is the value of the variable.
+	// The value can be any type.
+	tfvars := map[string]any{
 		"example_condition": true,
 	}
 
-	// Setup two tests!
-	testPresent, err := setuptest.Dirs(moduleDir, "").WithVars(vars).InitPlanShow(t)
-	require.NoError(t, err)
-	testNotPresent, err := setuptest.Dirs(moduleDir, "").WithVars(nil).InitPlanShow(t)
-	require.NoError(t, err)
-	defer testPresent.Cleanup()
-	defer testNotPresent.Cleanup()
+	// We are setting up two tests, one where the condition is true and one where it is false.
+	// We do this using subtests, created by the t.Run() function.
+	// This function takes two values: a name for the test and a function to run the test.
 
-	// Check that the resource is present in the test_present plan and that the input attribute has the value "example_condition"...
+	// The first test, where the condition is true.
+	t.Run("present", func(t *testing.T) {
+		// Setup the test using the Terraform variables defined above
+		test, err := setuptest.Dirs(moduleDir, "").WithVars(tfvars).InitPlanShow(t)
+		require.NoError(t, err)
+		defer test.Cleanup()
 
-	// Now check that the resource is NOT present in the test_notpresent plan...
+		// Check that the resource is present in the present test and that the input attribute has the value "example_condition"...
 
-	t.Fail() // remove this once you have done!
+		t.Fail() // remove this once you have done!
+	})
+
+	// This is the second test, where no variables are supplied and the condition is false due to the default value of the variable.
+	t.Run("notPresent", func(t *testing.T) {
+		// Setup the test using the Terraform variables defined above
+		test, err := setuptest.Dirs(moduleDir, "").WithVars(nil).InitPlanShow(t)
+		require.NoError(t, err)
+		defer test.Cleanup()
+
+		// Check that the resource is NOT present in the notPresent test...
+
+		t.Fail() // remove this once you have done!
+	})
 }
 
 func TestForEach(t *testing.T) {
 	t.Parallel()
 
-	// Define module variables.
-	// See https://go.dev/tour/methods/14 for a description of the empty interface.
-	// Search the internet for 'golang empty interface' for more examples.
-	vars := map[string]interface{}{
+	// Define module variables
+	// See https://go.dev/tour/methods/14 for a description of the any value (aka empty interface).
+	// This is a good article: https://bitfieldconsulting.com/golang/map-string-interface.
+	vars := map[string]any{
 		"example_for_each": map[string]string{
 			"instance_1": "data_1",
 			"instance_2": "data_2",
